3dfb0955ed1571e249bf7f23983af7f2d0e8d868
Andrew Fennell <afennell@hotmail.co.uk>
2023-10-30 20:54:25 +0000

    Lab 01 done

---
+++ src/components/LocationForm.jsx
@@ -0,0 +1,14 @@
+export default function LocationForm({ getLocation, handleChange }) {
+  return (
+    <form onSubmit={getLocation} className="locationForm">
+      <label for="locationInput">Enter your location:</label>
+      <input
+        onChange={handleChange}
+        type="text"
+        name="locationInput"
+        className="locationInput"
+      />
+      <button className="locationSubmit">SUBMIT</button>
+    </form>
+  );
+}
